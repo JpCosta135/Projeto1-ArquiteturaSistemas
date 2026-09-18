ff13f1924cb1ddfc3f6d95e29e6b894bf7ebdf3f
João Paulo <130411184+JpCosta135@users.noreply.github.com>
2026-09-18 17:46:46 -0300

    adicionado método put para preenchimento do inventário

--- src/Repositorio.java
+++ src/Repositorio.java
@@ -2,15 +2,17 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 
 public class Repositorio {
-    private Hashtable <Integer,Projetor> inventario;
+    private Hashtable <String,Projetor> inventario;
 
     public Repositorio () {
         inventario = new Hashtable<>();
         Projetor p1 = new Projetor("Epson PowerLite X49","PAT-100231",true);
+        inventario.put(p1.getCodigo(),p1);
         Projetor p2 = new Projetor("BenQ MS550","PAT-100233",false);
+        inventario.put(p2.getCodigo(),p2);
         Projetor p3 = new Projetor("ViewSonic PA503S","PAT-100235",true);
+        inventario.put(p3.getCodigo(),p3);
     }
-
 
     public Projetor getProjetor(int id){
         Projetor projetor = null;
@@ -18,8 +20,6 @@ public class Repositorio {
        return projetor;
 
     }
-
-
 
 
 
