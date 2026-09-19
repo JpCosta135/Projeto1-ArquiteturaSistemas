8cb8a4b233c6b36e39c1d3aecb5ffe979b8ce2cf
João Paulo <130411184+JpCosta135@users.noreply.github.com>
2026-09-19 17:28:47 -0300

    Refatoração do método atualizarUsuario

--- Projeto/src/Servico/CadastroUsuario.java
+++ Projeto/src/Servico/CadastroUsuario.java
@@ -10,24 +10,29 @@ public class CadastroUsuario {
         this.repositorio = repositorio;
     }
 
-    public static void efetuarCadastro(byte tipo,String nome,int matricula,String senha){
+    public static void efetuarCadastro(byte tipo, String nome, int matricula, String senha) {
         FactoryUsuario fabrica = new FactoryUsuario();
-        Usuario usuario = fabrica.createUsuario(tipo,nome,matricula,senha);
+        Usuario usuario = fabrica.createUsuario(tipo, nome, matricula, senha);
     }
-    public void removerUsuario(int matricula){
+
+    public void removerUsuario(int matricula) {
         if (repositorio.getUsuario(matricula) != null) {
             repositorio.removerUsuario(matricula);
         } else {
             System.out.println("Usuário não encontrado!");
         }
-     }
-    public void atualizarUsuario (int matricula,String nome,int matriculaNova,String senha) {
-        repositorio.setUsuario(matricula,nome,matriculaNova,senha);
     }
-    public void lerUsuario (int matricula) {
+
+    public void atualizarUsuario(int matricula, String nome, int matriculaNova, String senha) {
+        if (repositorio.getUsuario(matricula) != null) {
+            repositorio.setUsuario(matricula, nome, matriculaNova, senha);
+        } else {
+            System.out.println("Usuário não encontrado!");
+        }
+    }
+
+    public void lerUsuario(int matricula) {
         repositorio.getUsuario(matricula);
     }
-
-
 
 }
