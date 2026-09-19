97691d0e9bb6cc95400d477c32b2cc52e0e10c5d
João Paulo <130411184+JpCosta135@users.noreply.github.com>
2026-09-19 12:59:20 -0300

    Refatoração das classes Aplicacao e Controlador

--- src/Sistema/Aplicacao.java
+++ src/Sistema/Aplicacao.java
@@ -1,8 +1,11 @@
+import Sistema.Controlador;
 import Sistema.Repositorio;
 
 void main() {
 
     Repositorio repositorio = new Repositorio();
+    Controlador controlador = new  Controlador(repositorio);
+
 
 
 
--- src/Sistema/Controlador.java
+++ src/Sistema/Controlador.java
@@ -7,7 +7,9 @@ import Modelo.Projetor;
 public class Controlador {
     private Repositorio repositorio;
 
-
+    public Controlador(Repositorio repositorio) {
+        this.repositorio = repositorio;
+    }
 
     public void realizarEmprestimo(Professor p, Atendente atendente, Projetor projetor)  {
          atendente.realizarEmprestimo(p,projetor);
