f38cbac618ff3c4e7cb7a69425c86dac84bc791f
João Paulo <130411184+JpCosta135@users.noreply.github.com>
2026-09-19 17:14:58 -0300

    Refatoração da classe App

--- Projeto/src/App/Aplicacao.java
+++ Projeto/src/App/Aplicacao.java
@@ -3,12 +3,14 @@ import Controlador.ControladorUsuario;
 import Modelo.Atendente;
 import Modelo.Professor;
 import Repositorio.Repositorio;
+import Servico.CadastroUsuario;
 
 void main() {
 
     Repositorio repositorio = new Repositorio();
     ControladorAtendente c1 = new ControladorAtendente(repositorio,(Atendente)repositorio.getUsuario(2345786));
     ControladorUsuario c2 = new ControladorUsuario(repositorio,repositorio.getUsuario(2097453));
+    CadastroUsuario c3 = new CadastroUsuario(repositorio);
 
     c1.realizarEmprestimo((Professor)(repositorio.getUsuario(2097453)),repositorio.getProjetor("PAT-100235"));
     c2.realizarPermutacao((Professor)(repositorio.getUsuario(2234566)),repositorio.getProjetor("PAT-100235"));
