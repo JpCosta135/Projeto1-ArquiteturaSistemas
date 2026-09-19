3aa34e5db54045e350f417922dbf9d5972c86190
João Paulo <130411184+JpCosta135@users.noreply.github.com>
2026-09-19 15:23:25 -0300

    refatoração do metodo realizarPermutacao

--- Projeto/src/Sistema/ControladorUsuario.java
+++ Projeto/src/Sistema/ControladorUsuario.java
@@ -1,5 +1,7 @@
 package Sistema;
 
+import Modelo.Professor;
+import Modelo.Projetor;
 import Modelo.Usuario;
 
 public class ControladorUsuario {
@@ -19,7 +21,13 @@ public class ControladorUsuario {
         usuario.autenticar(senha);
     }
 
-
+    public void realizarPermutacao(Professor professor, Projetor projetor) {
+        if (usuario instanceof Professor) {
+            ((Professor) usuario).permutarProjetor(professor);
+        } else {
+            System.out.println("Usuário não possui permissão para realizar permutação");
+        }
+    }
 
 
 
