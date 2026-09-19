dc7dcc8164bb596a2d0f70e2b7ef8cf0ae5dafb9
João Paulo <130411184+JpCosta135@users.noreply.github.com>
2026-09-19 12:48:14 -0300

    Refatoração da classe FactoryUsuario

--- src/Classes/FactoryUsuario.java
+++ src/Classes/FactoryUsuario.java
@@ -6,7 +6,7 @@ import Modelo.Usuario;
 
 public class FactoryUsuario {
 
-    public Usuario createUsuario(int tipo, String nome, int matricula, String senha) {
+    public Usuario createUsuario(byte tipo, String nome, int matricula, String senha) {
         if (tipo != 1 || tipo != 2 ) {
             System.out.println("Tipo invalido, informe o valor 1 ou 2");
         }
