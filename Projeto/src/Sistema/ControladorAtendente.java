8b5fdbe2382474dc60cdb1198029010d4a2a17dc
João Paulo <130411184+JpCosta135@users.noreply.github.com>
2026-09-19 13:54:14 -0300

    elaboração das classes ControladorAtendente e ControladorUsuario

---
+++ Projeto/src/Sistema/ControladorAtendente.java
@@ -0,0 +1,26 @@
+package Sistema;
+
+import Modelo.Atendente;
+
+public class ControladorAtendente {
+    private Repositorio repositorio;
+    private Atendente atendente;
+
+    public ControladorAtendente(Repositorio repositorio, Atendente atendente) {
+        this.repositorio = repositorio;
+        this.atendente = atendente;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Projeto/src/Sistema/ControladorUsuario.java
@@ -0,0 +1,18 @@
+package Sistema;
+
+import Modelo.Usuario;
+
+public class ControladorUsuario {
+    private Repositorio repositorio;
+    private Usuario usuario;
+
+    public ControladorUsuario(Repositorio repositorio, Usuario usuario) {
+        this.repositorio = repositorio;
+        this.usuario = usuario;
+    }
+
+
+
+
+
+}
