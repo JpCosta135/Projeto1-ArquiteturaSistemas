f234cfd00a3ef397146a2004f1c47cc2cdf0f38f
João Paulo <130411184+JpCosta135@users.noreply.github.com>
2026-09-20 13:48:03 -0300

    adicionado comentários na classe Aplicacao

--- Projeto/src/App/Aplicacao.java
+++ Projeto/src/App/Aplicacao.java
@@ -12,7 +12,6 @@ void main() {
     ControladorAtendente c1 = new ControladorAtendente(repositorio,(Atendente)repositorio.getUsuario(2345786));
     ControladorUsuario c2 = new ControladorUsuario(repositorio,repositorio.getUsuario(2097453));
     CadastroUsuario c3 = new CadastroUsuario(repositorio);
-
     c1.realizarEmprestimo((Professor)(repositorio.getUsuario(2097453)),repositorio.getProjetor("PAT-100235"));
     c2.realizarPermutacao((Professor)(repositorio.getUsuario(2234566)),repositorio.getProjetor("PAT-100235"));
     c1.realizarDevolucao((Professor)(repositorio.getUsuario(2234566)),repositorio.getProjetor("PAT-100235"));
@@ -21,18 +20,18 @@ void main() {
     c3.efetuarCadastro((byte)1,"Armando Alves",1890298,"dfjo23",repositorio);
     System.out.println(c3.lerUsuario(1890298));
     c3.atualizarUsuario(1890298,"Armando Alves",1890298,"3737hr");
-    c3.removerUsuario(1890298);
+    c3.removerUsuario(1890298); //CRUD de professores
 
     Projetor p1 = new Projetor("BenQ MS550","PAT-100233",true);
     c1.realizarCadastroProjetor(p1);
     System.out.println(c1.lerProjetor("PAT-100233"));
     c1.atualizarProjetor("PAT-100233","BenQ MS550","PAT-100233",false);
-    c1.removerProjetor("PAT-100233");
+    c1.removerProjetor("PAT-100233"); //CRUD de projetores
 
     c3.efetuarCadastro((byte)2,"Caio Ferreira",1943675,"tbrt4",repositorio);
     System.out.println(c3.lerUsuario(1943675));
     c3.atualizarUsuario(1943675,"Caio Ferreira",1943675,"en3tb");
-    c3.removerUsuario(1943675);
+    c3.removerUsuario(1943675); //CRUD de atendentes
 
 
 
