a5c0ebfee47fbfa790333ba4394e9c27249ced0a
João Paulo <130411184+JpCosta135@users.noreply.github.com>
2026-09-22 13:54:38 -0300

    Refatoração do padrão Builder

--- Projeto/src/App/Aplicacao.java
+++ Projeto/src/App/Aplicacao.java
@@ -4,33 +4,36 @@ import Modelo.*;
 import Repositorio.Repositorio;
 import Servico.CadastroUsuario;
 
- void main() {
+void main() {
 
-    Repositorio repositorio = Repositorio.getInstancia();
-    ControladorAtendente c1 = new ControladorAtendente(repositorio,(Atendente)repositorio.getUsuario(2345786));
-    ControladorUsuario c2 = new ControladorUsuario(repositorio,repositorio.getUsuario(2097453));
-    CadastroUsuario c3 = new CadastroUsuario(repositorio);
-    c1.realizarEmprestimo((Professor)(repositorio.getUsuario(2097453)),repositorio.getProjetor("PAT-100235"));
-    c2.realizarPermutacao((Professor)(repositorio.getUsuario(2234566)),repositorio.getProjetor("PAT-100235"));
-    c1.realizarDevolucao((Professor)(repositorio.getUsuario(2234566)),repositorio.getProjetor("PAT-100235"));
-    c2.realizarAutenticacao("ebt325"); // Instanciação dos objetos e operações básicas do sistema
+   Repositorio repositorio = Repositorio.getInstancia();
+   ControladorAtendente c1 = new ControladorAtendente(repositorio, (Atendente) repositorio.getUsuario(2345786));
+   ControladorUsuario c2 = new ControladorUsuario(repositorio, repositorio.getUsuario(2097453));
+   CadastroUsuario c3 = new CadastroUsuario(repositorio);
+   c1.realizarEmprestimo((Professor) (repositorio.getUsuario(2097453)), repositorio.getProjetor("PAT-100235"));
+   c2.realizarPermutacao((Professor) (repositorio.getUsuario(2234566)), repositorio.getProjetor("PAT-100235"));
+   c1.realizarDevolucao((Professor) (repositorio.getUsuario(2234566)), repositorio.getProjetor("PAT-100235"));
+   c2.realizarAutenticacao("ebt325"); // Instanciação dos objetos e operações básicas do sistema
 
-    c3.efetuarCadastro((byte)1,"Armando Alves",1890298,"dfjo23",repositorio);
-    System.out.println(c3.lerUsuario(1890298));
-    c3.atualizarUsuario(1890298,"Armando Alves",1890298,"3737hr");
-    c3.removerUsuario(1890298); // CRUD de professores
+   c3.efetuarCadastro((byte) 1, "Armando Alves", 1890298, "dfjo23", repositorio);
+   System.out.println(c3.lerUsuario(1890298));
+   c3.atualizarUsuario(1890298, "Armando Alves", 1890298, "3737hr");
+   c3.removerUsuario(1890298); // CRUD de professores
 
-    c3.efetuarCadastro((byte)2,"Caio Ferreira",1943675,"tbrt4",repositorio);
-    System.out.println(c3.lerUsuario(1943675));
-    c3.atualizarUsuario(1943675,"Caio Ferreira",1943675,"en3tb");
-    c3.removerUsuario(1943675); //  CRUD de atendentes
+   c3.efetuarCadastro((byte) 2, "Caio Ferreira", 1943675, "tbrt4", repositorio);
+   System.out.println(c3.lerUsuario(1943675));
+   c3.atualizarUsuario(1943675, "Caio Ferreira", 1943675, "en3tb");
+   c3.removerUsuario(1943675); //  CRUD de atendentes
 
-    Projetor p1 = new Projetor("BenQ MS550","PAT-100233",true);
-    c1.realizarCadastroProjetor(p1);
-    System.out.println(c1.lerProjetor("PAT-100233"));
-    c1.atualizarProjetor("PAT-100233","BenQ MS550","PAT-100233",false);
-    c1.removerProjetor("PAT-100233"); //  CRUD de projetores
-
+   Projetor p1 = new Projetor.Builder()
+           .nome("BenQ MS550")
+           .codigo("PAT-100233")
+            .estadoInicial(EstadoProjetor.DISPONIVEL)
+           .build();
+   c1.realizarCadastroProjetor(p1);
+   System.out.println(c1.lerProjetor("PAT-100233"));
+   c1.atualizarProjetor("PAT-100233", "BenQ MS550", "PAT-100233", false);
+   c1.removerProjetor("PAT-100233"); //  CRUD de projetores
 
 
 }
